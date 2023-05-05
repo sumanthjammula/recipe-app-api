8bcc12b4d511c633deb3b7e60e0af9bbf1f15ffd
sumanthjammula <sumanthjammula@gmail.com>
2023-05-05 16:26:52 -0500

    Somechanges

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -2,7 +2,7 @@ import time
 from psycopg2 import OperationalError as Psycopg2Error
 
 from django.db.utils import OperationalError
-from django.core.management import BaseCommand
+from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -9,7 +9,7 @@ from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
 @patch('core.management.commands.wait_for_db.Command.check')
-class CommandTest(SimpleTestCase):
+class CommandTests(SimpleTestCase):
     
     def test_wait_for_db_ready(self, patched_check):
         patched_check.return_value =True
@@ -21,10 +21,10 @@ class CommandTest(SimpleTestCase):
     @patch('time.sleep')    
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         patched_check.side_effect = [Psycopg2Error] * 2 + \
-            [OperationalError] * 3 +[True]
+            [OperationalError] * 3 + [True]
         
         call_command('wait_for_db')
         
         self.assertEqual(patched_check.call_count,6)
-        patched_check.assert_called_once_with(databases=['default'])
+        patched_check.assert_called_with(databases=['default'])
             
